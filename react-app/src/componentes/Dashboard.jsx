42718ffada8598355ca98c752fba03987835e1ce
Kowshalyadev <kowshalyagurram@gmail.com>
2025-04-29 23:58:35 +0530

    react

---
+++ react-app/src/componentes/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Dashboard() {
+  return (
+    <div className='m-8 p-5'>
+      <h1 className="text-2xl font-bold">Dashboard</h1>
+      <p>This is the Dashboard page.</p>
+    </div>
+  );
+}
+
+export default Dashboard;
